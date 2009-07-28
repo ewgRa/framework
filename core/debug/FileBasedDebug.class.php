cad53c8b19167d1859c954885f80518f93a68936
Evgeniy Sokolov <ewgraf@gmail.com>
2009-07-28 17:44:42 +0000

    add template for filebaseddebug

---
+++ core/debug/FileBasedDebug.class.php
@@ -0,0 +1,13 @@
+<?php
+	/* $Id$ */
+	
+	/**
+	 * @license http://www.opensource.org/licenses/bsd-license.php BSD
+	 * @author Evgeniy Sokolov <ewgraf@gmail.com>
+	 *
+	 * // TODO: realize me
+	*/
+	final class FileBasedDebug extends BaseDebug
+	{
+	}
+?>
