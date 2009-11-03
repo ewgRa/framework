da36da033f8298bace4284f3010a07551eb5ff1c
Evgeniy Sokolov <ewgraf@gmail.com>
2009-11-03 15:16:25 +0000

    add chain controller test

--- tests/cases/patterns/ChainControllerTestCase.class.php
+++ tests/cases/patterns/ChainControllerTestCase.class.php
@@ -9,7 +9,50 @@
 	{
 		public function testHandleRequest()
 		{
-			$this->fail();
+			$chain =
+				new TestChainController1(
+					new TestChainController2()
+				);
+				
+			$mav =
+				ModelAndView::create()->
+				setModel(
+					Model::create()->
+					set('callStack', array())
+				);
+				
+			$chain->handleRequest(HttpRequest::create(), $mav);
+			
+			$this->assertSame(
+				$mav->getModel()->get('callStack'),
+				array('TestChainController1', 'TestChainController2')
+			);
 		}
+	}
+	
+	class BaseTestChainController extends ChainController
+	{
+		/**
+		 * @return ModelAndView
+		 */
+		public function handleRequest(
+			HttpRequest $request,
+			ModelAndView $mav
+		) {
+			$callStack = $mav->getModel()->get('callStack');
+			$callStack[] = get_class($this);
+
+			$mav->getModel()->set('callStack', $callStack);
+			
+			return parent::handleRequest($request, $mav);
+		}
+	}
+
+	final class TestChainController1 extends BaseTestChainController
+	{
+	}
+
+	final class TestChainController2 extends BaseTestChainController
+	{
 	}
 ?>
