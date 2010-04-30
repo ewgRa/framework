470dc98faf8ac10e8ca7d61f5f2cb7627e855277
Evgeniy Sokolov <ewgraf@gmail.com>
2010-04-30 16:45:06 +0000

    cosmetics

--- core/debug/FileBasedDebugDA.class.php
+++ core/debug/FileBasedDebugDA.class.php
@@ -1,11 +1,8 @@
 <?php
-	/* $Id$ */
-	
 	/**
 	 * @license http://www.opensource.org/licenses/bsd-license.php BSD
 	 * @author Evgeniy Sokolov <ewgraf@gmail.com>
-	 *
-	 * // TODO: realize me
+	 * TODO: realize me
 	*/
 	final class FileBasedDebugDA
 	{
--- core/view/ViewInterface.class.php
+++ core/view/ViewInterface.class.php
@@ -1,6 +1,4 @@
 <?php
-	/* $Id$ */
-
 	/**
 	 * @license http://www.opensource.org/licenses/bsd-license.php BSD
 	 * @author Evgeniy Sokolov <ewgraf@gmail.com>
