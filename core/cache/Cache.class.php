b10f494676b19103305e4f0f87835eb0646b3573
Evgeniy Sokolov <ewgraf@gmail.com>
2011-03-18 05:18:02 +0000

    swap pools

--- core/cache/Cache.class.php
+++ core/cache/Cache.class.php
@@ -28,6 +28,19 @@
 			return $this;
 		}
 		
+		/**
+		 * @return Cache
+		 */
+		public function swapPools($poolAlias, $swapPoolAlias)
+		{
+			$pool = $this->getPool($poolAlias);
+			$swapPool = $this->getPool($swapPoolAlias);
+			
+			$this->pools[$poolAlias] = $swapPool;
+			$this->pools[$swapPoolAlias] = $pool;
+			return $this;
+		}
+		
 		public function hasPool($poolAlias)
 		{
 			return isset($this->pools[$poolAlias]);
