e617bda35dd71e96f7ae674bd0a975d0bc8de9f2
Evgeniy Sokolov <e.sokolov@co.wapstart.ru>
2011-08-03 11:47:57 +0600

    add Browser class

---
+++ core/common/Browser.class.php
@@ -0,0 +1,31 @@
+<?php
+	namespace ewgraFramework;
+
+	/**
+	 * @license http://www.opensource.org/licenses/bsd-license.php BSD
+	 * @author Evgeniy Sokolov <ewgraf@gmail.com>
+	*/
+	final class Browser
+	{
+		private $userAgent = null;
+
+		public static function createFromUserAgent($userAgent)
+		{
+			$self = new self;
+
+			$self->setUserAgent($userAgent);
+
+			return $self;
+		}
+
+		public function isIE6()
+		{
+			return mb_stripos($this->userAgent, 'msie 6', 0, 'utf8');
+		}
+
+		private function setUserAgent($userAgent)
+		{
+			$this->userAgent = $userAgent;
+		}
+	}
+?>
