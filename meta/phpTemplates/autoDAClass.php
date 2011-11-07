de8066e3bf42a72e2a6ca8013d1bd29bbec1d2d1
Evgeniy Sokolov <e.sokolov@co.wapstart.ru>
2011-11-07 23:04:48 +0600

    fix postgresql in()

--- meta/phpTemplates/autoDAClass.php
+++ meta/phpTemplates/autoDAClass.php
@@ -231,6 +231,9 @@
 
 		public function getByIds(array $ids)
 		{
+			if (!$ids)
+				return array();
+
 			return $this->getListCachedByQuery(
 				\ewgraFramework\DatabaseQuery::create()->
 				setQuery('SELECT * FROM '.$this->getTable().' WHERE id IN(?)')->
